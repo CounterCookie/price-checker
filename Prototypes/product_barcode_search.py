ac6db39d3ab32a4712f406f8a7ebe115aaa541bd
Shayan Heidari <countercookio@gmail.com>
2023-06-10 01:41:06 -0700

    List only one simple product. Cant get variations yet.

--- Prototypes/product_barcode_search.py
+++ Prototypes/product_barcode_search.py
@@ -14,12 +14,24 @@ def search(barcode, key, secret):
     url = f"https://goodnaturehf.com/wp-json/wc/v3/products?search={barcode}&consumer_key={key}&consumer_secret={secret}"
     return json.loads(requests.get(url).text)
 
+def variation_search(barcode, wholeResult, key, secret):
+    for variation in wholeResult['variations']:
+        url = f"https://goodnaturehf.com/wp-json/wc/v3/products/{wholeResult['id']}/variations/{variation}?consumer_key={key}&consumer_secret={secret}" 
+        res = json.loads(requests.get(url).text)
+        pprint(res)
+
+
 
 def main():
     key, secret = read_creds()
-    
-    res = search("032917007384", key, secret)
-    pprint(res[0]['price'])
+    barcode = "628110035578"
+
+    res = search(barcode, key, secret)[0]
+   
+    if res['type'] == 'variable':
+        variation_search(barcode, res, key, secret)
+    else:
+        pprint(res)
 
 
 
