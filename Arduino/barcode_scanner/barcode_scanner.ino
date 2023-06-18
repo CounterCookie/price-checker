619db08fa7ab048e4ab3ff5ae212b7eb1a6cec9c
CounterCookie <countercookio@gmail.com>
2023-06-18 01:43:25 -0700

    Arduino barcode scanner demo

---
+++ Arduino/barcode_scanner/barcode_scanner.ino
@@ -0,0 +1,33 @@
+/*
+ * Simple barcode scanner bit. just to confirm that barcode scanner works.
+ * Connect TX, RX to D3 and D4 respectively
+ * 
+ */
+#include <HyperDisplay_SSD1309.h>
+#include <SoftwareSerial.h>
+
+#define BARCODE_TX D3
+#define BARCODE_RX D4
+SoftwareSerial barcode(BARCODE_RX, BARCODE_TX);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello dolly!");
+  barcode.begin(9600);
+}
+
+void loop(){
+
+
+  if (barcode.available())
+  {
+    Serial.println("data");
+    while (barcode.available())
+    {
+      char input = barcode.read();
+      Serial.print(input);
+      delay(5);
+    }
+    Serial.println();
+  }
+}
